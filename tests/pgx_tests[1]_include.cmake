25ad15c1c9506aceea6cb7c5caf6bb64829bfd73
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-06-21 19:18:19 +0300

    Delete tests/pgx_tests[1]_include.cmake

--- tests/pgx_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/pawkorchargin/Documents/life/code/paw-regex/tests/pgx_tests[1]_tests.cmake")
-  include("/Users/pawkorchargin/Documents/life/code/paw-regex/tests/pgx_tests[1]_tests.cmake")
-else()
-  add_test(pgx_tests_NOT_BUILT pgx_tests_NOT_BUILT)
-endif()
